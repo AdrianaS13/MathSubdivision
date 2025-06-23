c8892f5a1abf79ef6de1b2cc2a19459760a9adcb
AdrianaS13 <adriana.msalas13@gmail.com>
2025-06-23 11:14:11 +0200

    First commit

---
+++ Assets/Scripts/Polyline.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Polyline : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float zDepth = 0f;
+    public GameObject pointPrefab; // Prefab de la esfera
+
+    private List<Vector3> points = new List<Vector3>();
+    private List<GameObject> pointObjects = new List<GameObject>();
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        if (!mainCamera) mainCamera = Camera.main;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
+        lineRenderer.useWorldSpace = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AddPointFromMouseClick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ClearLine();
+        }
+    }
+
+    void AddPointFromMouseClick()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Mathf.Abs(mainCamera.transform.position.z - zDepth);
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+        worldPos.z = zDepth;
+
+        points.Add(worldPos);
+        UpdateLineRenderer();
+
+        if (pointPrefab != null)
+        {
+            GameObject point = Instantiate(pointPrefab, worldPos, Quaternion.identity);
+            pointObjects.Add(point);
+        }
+    }
+
+    void UpdateLineRenderer()
+    {
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPositions(points.ToArray());
+    }
+
+    void ClearLine()
+    {
+        points.Clear();
+        lineRenderer.positionCount = 0;
+
+        foreach (GameObject obj in pointObjects)
+        {
+            Destroy(obj);
+        }
+        pointObjects.Clear();
+    }
+
+    public List<Vector3> GetPoints()
+    {
+        return points;
+    }
+}
